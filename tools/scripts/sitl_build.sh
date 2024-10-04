e890a9917f605816761c3e2422ba58a91c2d82b2
Damien SIX <damien@robotsix.net>
2024-10-04 14:08:09 +0200

    put back topics required for identification

--- tools/scripts/sitl_build.sh
+++ tools/scripts/sitl_build.sh
@@ -35,11 +35,21 @@ git config --global --add safe.directory '*'
 # Also add the lines after the line "publications:" if not found
 #  - topic: /fmu/out/vehicle_thrust_setpoint
 #    type: px4_msgs::msg::VehicleThrustSetpoint
+#  - topic: /fmu/out/actuator_motors
+#    type: px4_msgs::msg::ActuatorMotors
+#  - topic: /fmu/out/vehicle_acceleration
+#    type: px4_msgs::msg::VehicleAcceleration
 # ------------------------------------------------------------------------------
 sed -i 's/# - topic: \/fmu\/out\/vehicle_angular_velocity/- topic: \/fmu\/out\/vehicle_angular_velocity/' ./src/modules/uxrce_dds_client/dds_topics.yaml
 sed -i 's/#   type: px4_msgs::msg::VehicleAngularVelocity/  type: px4_msgs::msg::VehicleAngularVelocity/' ./src/modules/uxrce_dds_client/dds_topics.yaml
 if ! grep -q "/fmu/out/vehicle_thrust_setpoint" ./src/modules/uxrce_dds_client/dds_topics.yaml; then
     sed -i '/publications:/a \  - topic: \/fmu\/out\/vehicle_thrust_setpoint\n    type: px4_msgs::msg::VehicleThrustSetpoint' ./src/modules/uxrce_dds_client/dds_topics.yaml
+fi
+if ! grep -q "/fmu/out/actuator_motors" ./src/modules/uxrce_dds_client/dds_topics.yaml; then
+    sed -i '/publications:/a \  - topic: \/fmu\/out\/actuator_motors\n    type: px4_msgs::msg::ActuatorMotors' ./src/modules/uxrce_dds_client/dds_topics.yaml
+fi
+if ! grep -q "/fmu/out/vehicle_acceleration" ./src/modules/uxrce_dds_client/dds_topics.yaml; then
+    sed -i '/publications:/a \  - topic: \/fmu\/out\/vehicle_acceleration\n    type: px4_msgs::msg::VehicleAcceleration' ./src/modules/uxrce_dds_client/dds_topics.yaml
 fi
 
 # ------------------------------------------------------------------------------
